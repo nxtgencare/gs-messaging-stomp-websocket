5c923608c8e0af0163ea00860bb759097594f7ff
jamie <jamie@checkin.tech>
2022-03-22 09:31:06 -0300

    Adding a few more file

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,13 +13,14 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/gs-guide-websocket');
+	document.cookie = "key=" + $("#token").val() + "; path=/; domain=.localhost; SameSite=Strict; Secure";
+    var socket = new WebSocket('ws://localhost:3002/stomp-web-socket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/greetings', function (greeting) {
-            showGreeting(JSON.parse(greeting.body).content);
+        stompClient.subscribe('/topic/messages', function (heartbeat) {
+            pulseHeart(heartbeat);
         });
     });
 }
@@ -32,12 +33,22 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+function sendMessage() {
+    stompClient.send(
+    	"/app/send-alert",
+    	{},
+    	JSON.stringify(
+    		{
+    			'fileName': "aaa",
+    			'bytesRead': 0,
+    			body: $("#body").val()
+			}
+    	)
+    );
 }
 
-function showGreeting(message) {
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+function pulseHeart(heartbeat) {
+    $("#messages").append("<tr><td>" + heartbeat + "</td></tr>");
 }
 
 $(function () {
@@ -46,6 +57,6 @@ $(function () {
     });
     $( "#connect" ).click(function() { connect(); });
     $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+    $( "#send" ).click(function() { sendMessage(); });
 });
 
